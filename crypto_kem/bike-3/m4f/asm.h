62ad7a102b2cd4ca9dae30218c149555bbb21bb5
myhoon <ope2527@gmail.com>
2025-04-17 20:29:10 +0900

    update

--- crypto_kem/bike-3/m4f/asm.h
+++
@@ -1,5 +0,0 @@
-// Created by Till Eifert
-// public domain
-
-void cshift_asm (uint32_t *array, int s);
-void shiftright_small_umlal (uint32_t *array1, int s, int a);
--- crypto_kem/hqc-128/m4f/reduce.h
+++
@@ -1,8 +0,0 @@
-#ifndef _REDUCE_H_
-#define _REDUCE_H_
-
-#include <stdint.h>
-
-void reduce(uint32_t *o, const uint32_t *a);
-
-#endif
